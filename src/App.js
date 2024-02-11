896256f405b025b644a485d667461dd5220838c9
vampi-niche <vampi.niche@gmail.com>
2024-02-11 12:51:26 -0700

    react startup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./components/ExpenseItem";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Let's Start</h2>
+               <ExpenseItem expense={expenses[0]}></ExpenseItem>
+               <ExpenseItem expense={expenses[1]}></ExpenseItem>
+               <ExpenseItem expense={expenses[2]}></ExpenseItem>
+               <ExpenseItem expense={expenses[3]}></ExpenseItem>
+
       </header>
     </div>
   );
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css'
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      {/* <div>{props.expense.date.toUTCString()}</div> */}
+      <ExpenseDate date={props.expense.date}/>
+      <div className='expense-item__description'>
+        <h2>{props.expense.title}</h2>
+        <div className='expense-item__price'>${props.expense.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
